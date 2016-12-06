e4b62064f0dfa2d3624c22bf225ba11bac955717
Brendan <byendanb@gmail.com>
2016-12-06 22:36:18 +0000

    Desired header printed to page

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    console.log(req.headers)
+    var ip = req.headers['x-forwarded-for']
+    var language = req.headers['accept-language']
+    language = language.split(";")[0].split(",")[0]
+    var software = req.headers['user-agent']
+    software = software.split("(")[1].split(")")[0]
+    var returnInfo = {
+        'ip': ip,
+        'language': language,
+        'software': software
+    }
+  res.send(JSON.stringify(returnInfo))
 })
 
 app.listen((process.env.PORT || 8080), function() {
